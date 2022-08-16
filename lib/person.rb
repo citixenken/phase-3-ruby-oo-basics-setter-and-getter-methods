cae19ec0562a602d3febe577b84305f4f727ed3c
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-16 23:42:27 +0300

    commit tree 04328c366bfa4bbbe8f587d9941d6ccddad2f42a

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    #name setter method
+    def name=(name)
+        @name = name
+    end
+    #job setter method
+    def job=(job)
+        @job = job
+    end
 
+
+    #name getter method
+    def name
+        @name
+    end
+    #job getter method
+    def job
+        @job
+    end
 end
+
+cleo = Person.new
+cleo.name = "Raskal"
+
+puts cleo.name
